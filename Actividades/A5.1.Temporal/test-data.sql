b2b0d99e70e26c3dc2783cdce6c34f5a6b565bde
ealex_mondra <ealejandro.mondragont@gmail.com>
2018-02-07 12:35:09 -0600

    A5.1.Temporal Final

--- Actividades/A5.1.Temporal/test-data.sql
+++ Actividades/A5.1.Temporal/test-data.sql
@@ -2,3 +2,10 @@ INSERT INTO poliza_usuario (NOMBRE,FECHA_NACIMIENTO,DIRECCION,INICIO_POLIZA,FIN_
 VALUES ('Enrique','1994-05-28','Colina de cahuas','2017-06-01','2018-06-01', 100.00, 2,2015,'SS2','Mazda', 234.00,'hkdbja87asdfb');
 
 UPDATE poliza_usuario SET DIRECCION = 'Colina de arecibo' WHERE NO_POLIZA = 2;
+
+UPDATE poliza_usuario SET COBERTURA = 1 WHERE NO_POLIZA = 2;
+UPDATE poliza_usuario SET COBERTURA = 3 WHERE NO_POLIZA = 2;
+
+select * from poliza_usuario
+select * from poliza_usuario_sys
+select * from poliza_usuario_sys_history
