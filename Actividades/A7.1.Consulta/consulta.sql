0a32aa4ddc34f41d496c1a3eb520d1e75fe6cbda
ealex_mondra <ealejandro.mondragont@gmail.com>
2018-02-07 15:42:13 -0600

    A7.1.Consulta

--- Actividades/A7.1.Consulta/consulta.sql
+++ Actividades/A7.1.Consulta/consulta.sql
@@ -1,8 +1,15 @@
 -- select * from Orders where Orders.customerNumber = 112
+-- select ProductLines.textDescription, Orders.customerNumber
+-- from Orders  
+-- join OrderDetails on OrderDetails.orderNumber = Orders.orderNumber 
+-- join Products on Products.productCode = OrderDetails.productCode
+-- join ProductLines on Products.productLine = ProductLines.productLine
+-- where Orders.customerNumber = 112
+
+explain
 select ProductLines.textDescription, Orders.customerNumber
 from Orders  
 join OrderDetails on OrderDetails.orderNumber = Orders.orderNumber 
 join Products on Products.productCode = OrderDetails.productCode
 join ProductLines on Products.productLine = ProductLines.productLine
 where Orders.customerNumber = 112
-
